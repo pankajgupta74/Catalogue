25844f2a670318370680138ecccdbc8ec09e2f0c
pankajgupta74 <pankaj_gupta74@yahoo.com>
2016-05-15 07:57:34 +0530

    Update CatalogueController.java

--- src/main/java/com/rbs/catalogue/controller/CatalogueController.java
+++ src/main/java/com/rbs/catalogue/controller/CatalogueController.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.rbs.catalogue.domain.Catalogue;
 import com.rbs.catalogue.domain.Item;
@@ -20,7 +21,7 @@ public class CatalogueController {
 	@Autowired
 	private CatalogueService service;
 	
-	
+	@CrossOrigin(origins = "http://webapp-dummycompany.rhcloud.com")
 	@RequestMapping("/items")
     public List<Catalogue> items(){
 		
